611b2f3b754d9295773229dbf138213b03152c2b
mnl-assasin <mykelneds.03@gmail.com>
2017-02-09 18:28:17 +0800

    commit

---
+++ app/src/main/java/com/olfu/meis/activity/AppGuideActivity.java
@@ -0,0 +1,15 @@
+package com.olfu.meis.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.olfu.meis.R;
+
+public class AppGuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_guide);
+    }
+}
